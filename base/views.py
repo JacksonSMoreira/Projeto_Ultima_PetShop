3c36fa6d13d3a15fa7a8b7a65b205d15e8fc5f2a
Jackson-moreira-DevPrograma <ribsjackson@gmail.com>
2023-04-09 16:45:42 +0100

    Atualização bootstrap5

--- base/views.py
+++ base/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from base.forms import ContatoForm
 
 def inicio(request):
     return render(request, 'inicio.html')
 
 def contato(request):
-    return render(request, 'contato.html')
+    sucesso = False
+    if request.method =='GET':
+        form = ContatoForm()
+    else:
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+            sucesso = True
+    contexto = {
+        'telefone': '(99) 99999.9999',
+        'responsavel': 'Jackson Moreira',
+        'form': form,
+        'sucesso': sucesso
+        }    
+    return render(request, 'contato.html', contexto)
